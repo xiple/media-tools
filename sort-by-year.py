78338fceaace7fcc455a35347703409ccc694073
Loic Petit <loic.petit@ikmail.com>
2025-08-01 13:20:35 +0200

    Added script to sort pictures based on exif metadata's year

---
+++ sort-by-year.py
@@ -0,0 +1,94 @@
+import logging
+import argparse
+import os
+
+import piexif
+
+
+def get_filepaths(path, recursive):
+    all_filepaths = []
+    if not recursive:
+        abspath = os.path.abspath(path)
+        all_filepaths += [
+            (abspath, f)
+            for f in os.listdir(abspath)
+            if os.path.isfile(os.path.join(abspath, f))
+        ]
+    else:
+        for dirpath, _, filenames in os.walk(path):
+            abspath = os.path.abspath(dirpath)
+            all_filepaths += [(abspath, f) for f in filenames]
+    return all_filepaths
+
+
+def filter_filepaths(filepaths, allowed_ext):
+    return [(fp, fn) for fp, fn in filepaths if os.path.splitext(fn)[-1] in allowed_ext]
+
+
+def main(rootpath, recursive):
+    if not os.path.exists(rootpath):
+        raise FileNotFoundError("Path specified does not exist")
+
+    if not os.path.isdir(rootpath):
+        raise NotADirectoryError("Path specified is not a directory")
+
+    logger.info("Listing files in target directory")
+    filepaths = get_filepaths(rootpath, recursive)
+    logger.info(f"Total files: {len(filepaths)}")
+
+    allowed_extensions = set([".jpg", ".jpeg"])
+    logger.info(f"Filtering for valid file extensions: {allowed_extensions}")
+    filepaths = filter_filepaths(filepaths, allowed_ext=allowed_extensions)
+    num_files = len(filepaths)
+    logger.info(f"Valid files: {num_files}")
+
+    logger.info("Begin processing files")
+    for path, filename in filepaths:
+        filepath=os.path.join(path, filename)
+        targetDirectoryPath=""
+
+        try:
+            exif_dict = piexif.load(filepath)
+            dateTimeOriginal=exif_dict['Exif'].get(piexif.ExifIFD.DateTimeOriginal)
+            if dateTimeOriginal:
+                yearOriginal=dateTimeOriginal.decode('utf-8')[:4]
+                targetDirectory=os.path.join(rootpath, yearOriginal)
+
+                if not os.path.exists(targetDirectory):
+                    os.mkdir(targetDirectory)
+                    logger.info(f"Created {targetDirectory}")
+            else:
+                targetDirectory=os.path.join(rootpath, "Unknown")
+                if not os.path.exists(targetDirectory):
+                    os.mkdir(targetDirectory)
+                    logger.info(f"Created {targetDirectory}")
+
+            targetDirectoryPath=os.path.join(targetDirectory, filename)
+            os.rename(filepath, targetDirectoryPath)
+
+            logger.info(f"Moved {filepath} to {targetDirectoryPath}")
+        except piexif.InvalidImageDataError:
+            logger.warning("Invalid image data, skipping")
+            continue
+
+    logger.info("Finished processing files")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=("TODOTODO"))
+    parser.add_argument("rootpath", type=str, help="Path to pictures folder")
+    parser.add_argument(
+        "-r",
+        "--recursive",
+        default=False,
+        action="store_true",
+        help="Recursively process media",
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s %(name)s %(levelname)s: %(message)s"
+    )
+    logger = logging.getLogger("sort-by-year")
+
+    main(args.rootpath, args.recursive)
